21153dd2ba6b653c6c643bee52fe898fcf67cea9
Holaaa <marcosvintatge@gmail.com>
2026-04-05 20:31:15 +0200

    server.js

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ app.post('/enviar-email', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('🚀 Servidor funcionando en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
 });
